a93c951fb5dd67169d5d18c91fa8170a5657da00
rabab003 <bintaliraq4@gmail.com>
2025-03-24 14:43:48 +0300

    2:43 pm

--- src/Pages/index/index.jsx
+++ src/Pages/index/index.jsx
@@ -4,6 +4,8 @@ import Nav from "../../Components/Nav/Nav";
 import heroImage from "../../assets/How to Create Website in Tailwind css/rev_home3_01.png"
 import elemnt1 from '../../assets/How to Create Website in Tailwind css/h5_decor4.png'
 import elemnt2 from "../../assets/How to Create Website in Tailwind css/rev_home3_04.png"
+import elemnt3 from "../../assets/How to Create Website in Tailwind css/rev_home3_05.png"
+import elemnt4 from "../../assets/How to Create Website in Tailwind css/rev_home3_06.png"
 
 export default function index() {
   return (
@@ -11,19 +13,27 @@ export default function index() {
         <Nav/>
 
         <div className='w-full px-[12%] pt-[100px] flex flex-col lg:flex-row gap-[20px]'>
-          <div className='flex flex-col w-[50%] items-start gap-4 justify-center'>
+          <div className='flex flex-col lg:w-[50%] w-full  items-start gap-4 justify-center'>
             <small className='text-4xl title font-dancing'>best seller</small>
             <h2 className='text-7xl 2xl:text-8xl font-medium text-red-600 font-Kalnia'>ice cream <br /> novelties</h2>
             <p className='text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat est similique soluta perferendis, doloribus repellendus fugiat voluptates ducimus, esse consectetur accusantium, repellat temporibus neque eius asperiores dolore dolorum deserunt? Recusandae.</p>
           <button className='btn header-btn'>shop now <i className='ri-arrow-right-line'></i></button>
           </div>
-          <div className='w-[50%] flex items-center justify-center z-[1] relative right-0 bottom-0'>
+          <div className='lg:w-[50%] w-full flex items-center justify-center z-[1] relative right-0 bottom-0'>
             <img src={heroImage} alt="" className='w-full sm:w-[400px] 2xl:w-[600px]' />
           </div>
 
           <img src={elemnt1} alt="" className='w-[80px] h-[80px] absolute -top-[-80%] left-5' />
           <img src={elemnt2} alt="" className='w-[80px] h-[80px] absolute -top-[-20%] lg:left-[90%] left-[65%] hidden sm:flex' />
+          <img src={elemnt3} alt="" className='w-[90px] h-[90px] absolute -top-[-10%] left-5' />
+          <img src={elemnt4} alt="" className='w-[90px] h-[90px] absolute -top-[-85%] sm:-top-[-85%] lg:left-[85%] left-[-75%]' />
         </div>
+
+        {/* category section */}
+
+        <section>
+
+        </section>
       
     </div>
   )
@@ -31,4 +41,4 @@ export default function index() {
 
 
 
-// 47 min in the video project (adding the third img (element3))
+// 52  (adding the second section)
